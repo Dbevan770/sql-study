972c910735736549c43ed9617b8a57dec915ce9f
Daniel Bevan <20234823+Dbevan770@users.noreply.github.com>
2024-03-09 16:59:16 -0600

    Day 1 Study session code

---
+++ Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.Data.SqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching"
+};
+
+string connectionString =
+    "Server=localhost,1433;Database=master;User Id=sa;Password=MyV3ryStr0ngPass;Encrypt=true;TrustServerCertificate=True";
+
+app.MapPost(
+    "/todos",
+    (string payload) =>
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            Console.WriteLine("Connected to SQL Server");
+            Console.WriteLine("Payload: " + payload);
+            connection.Close();
+            Console.WriteLine("Disconnected from SQL Server");
+        }
+
+        return "Hello World";
+    }
+);
+
+// HTTP GET -> /weatherforecast
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast =  Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast")
+// .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
